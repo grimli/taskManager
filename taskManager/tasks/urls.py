b8bcbc62941601c668f89e5409be0fbe81752d94
Edoardo Bompiani <edoardo.bompiani@immobiliare.it>
2018-03-05 23:02:00 +0100

    added basic task detail

--- taskManager/tasks/urls.py
+++ taskManager/tasks/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 ]
