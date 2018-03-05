d0f0ce8775a363e648c9ae99570cf05fdcd7f495
Edoardo Bompiani <edoardo.bompiani@immobiliare.it>
2018-03-05 22:44:54 +0100

    Some fix on model, added basic list view

--- taskManager/tasks/models.py
+++ taskManager/tasks/models.py
@@ -5,25 +5,34 @@ from django.utils import timezone
 class Priority( models.Model ):
     label = models.CharField(max_length=20, unique=True)
     value = models.IntegerField(unique=True)
+    
+    def __str__(self):
+        return self.label
 
 
 class Status( models.Model ):
     label = models.CharField(max_length=20, unique=True)
     value = models.IntegerField(unique=True)
+    
+    def __str__(self):
+        return self.label
 
 
 class Task( models.Model ):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=8000)
-    fk_parent = models.ForeignKey('self', on_delete=models.CASCADE)
-    fk_priority = models.ForeignKey(Priority, on_delete=models.PROTECT )
-    fk_status = models.ForeignKey(Status, on_delete=models.PROTECT)
+    fk_parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True, default=None )
+    fk_priority = models.ForeignKey(Priority, on_delete=models.PROTECT, blank=True, null=True, default=None )
+    fk_status = models.ForeignKey(Status, on_delete=models.PROTECT, blank=True, null=True, default=None)
     creation_date = models.DateField(default = timezone.now) # creation date for the task record
-    ending_date = models.DateField(default = timezone.now) # Planned closing date for the task
-    closing_date = models.DateField(default = timezone.now) # closing date for the task
+    ending_date = models.DateField( null=True, blank=True, default=None) # Planned closing date for the task
+    closing_date = models.DateField( null=True, blank=True, default=None) # closing date for the task
     modification_date = models.DateField(default = timezone.now) # last modification date
     # Tag management later
     # Assegnazione task later
+    
+    def __str__(self):
+        return self.name
 
 
 class Note( models.Model ):
--- taskManager/tasks/views.py
+++ taskManager/tasks/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.utils import timezone
+from django.views import generic
 
+from .models import Note, Priority, Status, Task
 # Create your views here.
+
+class IndexView(generic.ListView):
+    model = Task
+
+    template_name = 'tasks/index.html'
+    context_object_name = 'active_task_list'
+
+    def get_queryset(self):
+        """Return active tasks."""
+        return Task.objects.order_by('-creation_date')
+
+
+#class ResultsView(generic.DetailView):
+#    model = Question
+#    template_name = 'polls/results.html'
+#
+#def vote(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    try:
+#        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+#    except (KeyError, Choice.DoesNotExist):
+#        # Redisplay the question voting form.
+#        return render(request, 'polls/detail.html', {
+#            'question': question,
+#            'error_message': "You didn't select a choice.",
+#        })
+#    else:
+#        selected_choice.votes += 1
+#        selected_choice.save()
+#        # Always return an HttpResponseRedirect after successfully dealing
+#        # with POST data. This prevents data from being posted twice if a
+#        # user hits the Back button.
+#        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
